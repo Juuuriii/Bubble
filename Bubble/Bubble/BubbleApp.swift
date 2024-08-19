f371788ef409842af6d9370c5c1f394db8cb3513
Juuuriii <jurihuhn@gmx.de>
2024-08-19 10:41:27 +0200

    First Commit

---
+++ Bubble/Bubble/BubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleApp.swift
+//  Bubble
+//
+//  Created by Juri Huhn on 19.08.24.
+//
+
+import SwiftUI
+
+@main
+struct BubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
