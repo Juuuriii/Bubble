2618c9f176e90e41d0fe68d30c18fa4acb88aedd
Juuuriii <jurihuhn@gmx.de>
2024-08-21 14:20:59 +0200

    Created FirestoreClient
    Implemented -> createUser, getUser

--- Bubble/Bubble/FireBaseClients/FirestoreClient.swift
+++ Bubble/Bubble/FireBaseClients/FirestoreClient.swift
@@ -8,4 +8,30 @@
 import Foundation
 import FirebaseFirestore
 
-
+class FirestoreClient {
+    
+    static let shared = FirestoreClient()
+    
+    private let store = Firestore.firestore()
+    
+    func createUser(uid: String, email: String, username: String) throws {
+        let user = BubbleUser(id: uid, email: email, username: username)
+        
+        try store.collection("users")
+            .document(uid)
+            .setData(from: user)
+    }
+    
+    func getUser(uid: String) async -> BubbleUser? {
+        
+        return try? await store
+            .collection("users")
+            .document(uid)
+            .getDocument()
+            .data(as: BubbleUser.self)
+        
+    }
+    
+    
+    
+}
---
+++ Bubble/Bubble/Models/BubbleUser.swift
@@ -0,0 +1,16 @@
+//
+//  BubbleUser.swift
+//  Bubble
+//
+//  Created by Juri Huhn on 21.08.24.
+//
+
+import Foundation
+
+struct BubbleUser: Codable {
+    
+    let id: String
+    let email: String
+    let username: String
+    
+}
