c15349f21fb9603a0f7948815d9eb33c412419af
Juuuriii <jurihuhn@gmx.de>
2024-08-21 14:07:05 +0200

    created AuthClient
    Implemented -> Register, Login, Logout, Delete Account

---
+++ Bubble/Bubble/FireBaseClients/AuthClient.swift
@@ -0,0 +1,38 @@
+//
+//  AuthClient.swift
+//  Bubble
+//
+//  Created by Juri Huhn on 21.08.24.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthClient {
+    
+    static let shared = AuthClient()
+    private let auth = Auth.auth()
+    
+    func register(email: String, password: String) async throws -> FirebaseAuth.User {
+        
+        let result = try await auth.createUser(withEmail: email, password: password)
+        return result.user
+        
+    }
+    
+    func login(email: String, password: String) async throws -> FirebaseAuth.User {
+        
+        let result = try await auth.signIn(withEmail: email, password: password)
+        return result.user
+    }
+    
+    func logout() throws {
+        try auth.signOut()
+    }
+    
+    func deleteUser() async throws {
+        if let user = auth.currentUser {
+            try await user.delete()
+        }
+    }
+}
---
+++ Bubble/Bubble/FireBaseClients/FirestoreClient.swift
@@ -0,0 +1,11 @@
+//
+//  FirestoreClient.swift
+//  Bubble
+//
+//  Created by Juri Huhn on 21.08.24.
+//
+
+import Foundation
+import FirebaseFirestore
+
+
