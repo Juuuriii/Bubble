39fa84c0aa9cd7e7d4a039ce8d5b7696a03293dc
Juuuriii <jurihuhn@gmx.de>
2024-08-23 14:27:04 +0200

    README Update

--- Bubble/Bubble/Views/AuthView/AuthView.swift
+++ Bubble/Bubble/Views/AuthView/AuthView.swift
@@ -6,44 +6,16 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct AuthView: View {
     
-   // @StateObject var viewModel = AuthViewModel()
+  //  @StateObject var viewModel = AuthViewModel()
     
     var body: some View {
         VStack{
-            Image("logo")
-            
-            ZStack{
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                    
-                    .foregroundStyle(.white)
-                
-                VStack{
-                    HStack {
-                        Spacer()
-                        Button("Sign In") {
-                            
-                        }
-                        .tint(Color(hex: "#14135B"))
-                        Spacer()
-                        Button("Sign Up") {
-                            
-                        }
-                        .tint(Color(hex: "#14135B"))
-                        Spacer()
-                    }
-                    TextField("Email", text: .constant(""))
-                    
-                    Spacer()
-                }
-            }
-            .padding(.horizontal, 24)
             
         }
-        .frame(maxWidth: .infinity ,maxHeight: .infinity)
-        .background(Color.blue.opacity(0.2))
     }
 }
 
