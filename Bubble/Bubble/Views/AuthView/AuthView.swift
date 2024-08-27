9200a8861e7a3a1bf9b7a000241141f4476bf59e
Juuuriii <jurihuhn@gmx.de>
2024-08-27 12:33:38 +0200

    Update AuthView

--- Bubble/Bubble/Views/AuthView/AuthView.swift
+++ Bubble/Bubble/Views/AuthView/AuthView.swift
@@ -14,8 +14,51 @@ struct AuthView: View {
     
     var body: some View {
         VStack{
-            Text("Hello")
+            Image("logo")
+            
+            ZStack{
+                
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                VStack{
+                    HStack{
+                        Button("Sign in"){
+                            
+                        }
+                        .foregroundStyle(.blue)
+                        
+                        Button("Sign up"){
+                            
+                        }
+                        .foregroundStyle(.blue)
+                    }
+                    
+                    ScrollView(.horizontal) {
+                        VStack() {
+                            TextField("Email", text: .constant(""))
+                                .textFieldStyle(.roundedBorder)
+                            
+                            SecureField("Password", text: .constant(""))
+                                .textFieldStyle(.roundedBorder)
+                            
+                            Button{
+                                
+                            } label: {
+                                Text("Sign in")
+                            }
+                            .buttonStyle(.borderedProminent)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background(.orange)
+                    }
+                    .background(.red)
+                    
+                }
+            }
+            .padding()
+            .foregroundStyle(.white)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.blue.opacity(0.2))
     }
 }
 
