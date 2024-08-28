1b402c1daeca830438d18f8d74fa2f510aff9122
Juuuriii <jurihuhn@gmx.de>
2024-08-28 11:15:03 +0200

    AuthView Update

--- Bubble/Bubble/Views/AuthView/AuthView.swift
+++ Bubble/Bubble/Views/AuthView/AuthView.swift
@@ -12,7 +12,8 @@ struct AuthView: View {
     
     @StateObject var viewModel = AuthViewModel()
    
-    @State var selectedTab = 1
+    @State var scrollPosition = 0
+    let colors: [Color] = [.red, .green, .blue]
     
     var body: some View {
         
@@ -24,55 +25,77 @@ struct AuthView: View {
                     .foregroundStyle(.white)
                   //  .frame(width: 0.9*proxy.size.width, height: 0.4*proxy.size.height)
                     .overlay{
-                        VStack{
-                            HStack{
-                                Button("Sign in"){
-                                    withAnimation{
-                                        selectedTab = 1
-                                    }
-                                }
-                                .padding(.vertical)
-                                Spacer()
-                                    .frame(width: 120)
-                                Button("Sign up"){
-                                    withAnimation{
-                                        selectedTab = 2
-                                    }
-                                }
-                                .padding(.vertical)
-                            }
-                            TabView(selection: $selectedTab){
-                                
-                                VStack(spacing: 24){
-                                    TextField("Email", text: $viewModel.email)
-                                        .bubbleTextFieldStyle()
-                            
-                                    SecureField("Password", text: $viewModel.password)
-                                        .bubbleTextFieldStyle()
-                                
-                                    Button("Sign in") {
+                        GeometryReader { proxy in
+                            VStack{
+                                HStack{
+                                    Button("Sign in"){
+                                        
+                                            scrollPosition = 0
                                         
                                     }
-                                    .authButtonStyle()
-                                    
-                                }
-                                .tag(1)
-                                VStack(spacing: 24){
-                                    TextField("Email", text: $viewModel.email)
-                                        .bubbleTextFieldStyle()
-                            
-                                    SecureField("Password", text: $viewModel.password)
-                                        .bubbleTextFieldStyle()
-                                    
-
-                                    Button("Sign up") {
+                                    .padding(.vertical)
+                                    Spacer()
+                                        .frame(width: 120)
+                                    Button("Sign up"){
+                                        
+                                            scrollPosition = 1
+                                        
                                     }
-                                    .authButtonStyle()
+                                    .padding(.vertical)
                                 }
-                                .tag(2)
+                                ScrollViewReader { value in
+                                   
+                                    
+                                    ScrollView(.horizontal){
+                                       
+                                        LazyHStack{
+                                            VStack(spacing: 24){
+                                                TextField("Email", text: $viewModel.email)
+                                                    .bubbleTextFieldStyle()
+                                                
+                                                SecureField("Password", text: $viewModel.password)
+                                                    .bubbleTextFieldStyle()
+                                                
+                                                Button("Sign in") {
+                                                    
+                                                }
+                                                .authButtonStyle()
+                                                
+                                            }
+                                            .id(0)
+                                            .frame(width: proxy.size.width)
+                                            
+                                            VStack(spacing: 24){
+                                                TextField("Email", text: $viewModel.email)
+                                                    .bubbleTextFieldStyle()
+                                                
+                                                SecureField("Password", text: $viewModel.password)
+                                                    .bubbleTextFieldStyle()
+                                                
+                                                
+                                                Button("Sign up") {
+                                                }
+                                                .authButtonStyle()
+                                            }
+                                            .id(1)
+                                            .frame(width: proxy.size.width)
+                                            
+                                        }
+                                        
+                                        .scrollTargetLayout()
+                                    }
+                                    .scrollTargetBehavior(.viewAligned)
+                                    .onChange(of: scrollPosition) {
+                                        withAnimation{
+                                            value.scrollTo(scrollPosition, anchor: .center)
+                                        }
+                                    }
+                                    .scrollDisabled(true)
+                                    
+                                }
+                                
                                 
                             }
-                            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
                         }
                     }
                     .padding()
