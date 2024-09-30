a340642da0e7b214c4b62b83da6e89a8f37137d9
Juuuriii <jurihuhn@gmx.de>
2024-09-30 15:26:59 +0200

    Update

---
+++ Bubble/Bubble/Components/Test.swift
@@ -0,0 +1,20 @@
+//
+//  Test.swift
+//  Bubble
+//
+//  Created by Juri Huhn on 30.09.24.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        AchievementBackgroundShape()
+            .stroke(lineWidth: 3)
+            .padding()
+    }
+}
+
+#Preview {
+    Test()
+}
